2fadfa2711b9a59d83edcb568fe61f6d65f13d24
Ayush0115D <dhakreayush578@gmail.com>
2025-04-15 22:34:39 +0530

    some updations

--- switchcase.java
+++ switchcase.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class Conditions {
+public class switchcase {
    public static void main(String args[]) {
        Scanner sc = new Scanner(System.in);
        int a = sc.nextInt();
