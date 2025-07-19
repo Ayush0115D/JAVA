8844aa1b23b86795c2f810d5337c4af06f8bdcde
Ayush0115D <dhakreayush578@gmail.com>
2025-07-19 23:33:43 +0530

    fixed bugs

--- clearbit.java
+++ clearbit.java
@@ -1,11 +1,12 @@
  import java.util.*;
-public class Bits {
+public class clearbit {
   public static void main(String args[]) {
-     int n = 5; //0101
+    int n = 5; //0101
      int pos = 2;
      int bitMask = 1<<pos;
      int newBitMask = ~(bitMask);
      int newNumber = newBitMask & n;
      System.out.println(newNumber);
   }
+
 }
